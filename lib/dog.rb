c9d8dda10ce1f030d00abf330bb43bddcba4b4c3
Kevin Huang <yellow.khaos@gmail.com>
2018-02-06 01:47:04 +0000

    automatically backed up by learn

---
+++ lib/dog.rb
@@ -0,0 +1,56 @@
+
+class Dog
+
+  attr_accessor :name, :breed
+  attr_reader :id
+
+  def initialize (id: nil, name:, breed:)
+    @name = name
+    @breed = breed
+    @id = id
+  end
+
+  def self.create_table
+    sql = <<-SQL
+      CREATE TABLE IF NOT EXISTS dogs (
+        id INTEGER PRIMARY key,
+        name TEXT,
+        breed TEXT
+      )
+      SQL
+
+    DB[:conn].execute(sql)
+  end
+
+  def self.drop_table
+    sql = <<-SQL
+      DROP TABLE dogs
+      SQL
+
+    DB[:conn].execute(sql)
+  end
+
+  def save
+    if self.id
+      self.update
+    else
+      sql = <<-SQL
+        INSERT INTO dogs (name, breed)
+        VALUES (?,?)
+        SQL
+      DB[:conn].execute(sql,self.name,self.breed)
+
+      @id = DB[:conn].execute("SELECT last_insert_rowid() FROM dogs")
+    end
+    self
+  end
+
+  def update
+    sql = <<-SQL
+      UPDATE dogs SET name = ?, breed = ?
+      WHERE id = ?
+      SQL
+    DB[:conn].execute(sql,self.name, self.breed, self.id)
+  end
+
+end
